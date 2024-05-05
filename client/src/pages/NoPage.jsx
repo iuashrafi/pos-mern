e068138c0aecb4ca3dbc8b3bdf98516576445741
Imtiaz Uddin <iuashrafi@gmail.com>
2024-05-05 20:02:00 +0530

    error page 404

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -1,5 +1,10 @@
 const NoPage = () => {
-  return <div className="p-16">ERROR 404 : Page does not exists</div>;
+  return (
+    <div className="p-16 flex flex-col items-center">
+      <div className="font-semibold text-6xl">ERROR 404</div>
+      <div className="font-semibold text-xl mt-6">Page not found</div>
+    </div>
+  );
 };
 
 export default NoPage;
